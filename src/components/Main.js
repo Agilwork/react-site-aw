2b1ebbf706cfe7a83c383da9a2c88fe514e69cbf
Diego <diego_henriquerodrigues@hotmail.com>
2018-03-08 11:04:41 -0300

    Created class team

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,6 +32,41 @@ class Customer extends Component {
   }
 }
 
+class Team extends Component {
+  render () {
+    return (
+      <section id="team" class="section section-center">
+        <h2>Nossa equipe</h2>
+        <p class="subtitle">Lorem ipsum dolor sit amet. Proin gravida nibh vel velit auctor aliquet</p>
+        <div class="container">
+          <div class="row">
+            <div class="col col-offset-desktop-1 col-desktop-2">
+              <div class="img-box-round"><img src="images/team/team1.jpg" alt="" /></div>
+              <p class="img-box-label">Lorem Ipsum</p>
+            </div>
+            <div class="col col-desktop-2">
+              <div class="img-box-round"><img src="images/team/team2.jpg" alt="" /></div>
+              <p class="img-box-label">Lorem Ipsum</p>
+            </div>
+            <div class="col col-desktop-2">
+              <div class="img-box-round"><img src="images/team/team3.jpg" alt="" /></div>
+              <p class="img-box-label">Lorem Ipsum</p>
+            </div>
+            <div class="col col-desktop-2">
+              <div class="img-box-round"><img src="images/team/team4.jpg" alt="" /></div>
+              <p class="img-box-label">Lorem Ipsum</p>
+            </div>
+            <div class="col col-desktop-2">
+              <div class="img-box-round"><img src="images/team/team5.jpg" alt="" /></div>
+              <p class="img-box-label">Lorem Ipsum</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
 class Main extends Component {
   render () {
     return (
@@ -71,35 +106,7 @@ class Main extends Component {
         </section>
 
         <Customer />
-
-        <section id="team" class="section section-center">
-          <h2>Nossa equipe</h2>
-          <p class="subtitle">Lorem ipsum dolor sit amet. Proin gravida nibh vel velit auctor aliquet</p>
-          <div class="container">
-            <div class="row">
-              <div class="col col-offset-desktop-1 col-desktop-2">
-                <div class="img-box-round"><img src="images/team/team1.jpg" alt="" /></div>
-                <p class="img-box-label">Lorem Ipsum</p>
-              </div>
-              <div class="col col-desktop-2">
-                <div class="img-box-round"><img src="images/team/team2.jpg" alt="" /></div>
-                <p class="img-box-label">Lorem Ipsum</p>
-              </div>
-              <div class="col col-desktop-2">
-                <div class="img-box-round"><img src="images/team/team3.jpg" alt="" /></div>
-                <p class="img-box-label">Lorem Ipsum</p>
-              </div>
-              <div class="col col-desktop-2">
-                <div class="img-box-round"><img src="images/team/team4.jpg" alt="" /></div>
-                <p class="img-box-label">Lorem Ipsum</p>
-              </div>
-              <div class="col col-desktop-2">
-                <div class="img-box-round"><img src="images/team/team5.jpg" alt="" /></div>
-                <p class="img-box-label">Lorem Ipsum</p>
-              </div>
-            </div>
-          </div>
-        </section>
+        <Team />        
       </main>
     ) 
   }
